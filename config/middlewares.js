8825fd94ec5cac3434eb70dfe8e18822621367b5
Khizr Abbas <khizer@example.com>
2025-09-18 00:33:32 +0500

    files

--- config/middlewares.js
+++ config/middlewares.js
@@ -8,8 +8,9 @@ module.exports = [
         useDefaults: true,
         directives: {
           'connect-src': ["'self'", "https:"],
-          'img-src': ["'self'", "data:", "blob:", "https:"],
-          'media-src': ["'self'", "data:", "blob:", "https:"],
+          'img-src': ["'self'", "data:", "blob:", "https:", "res.cloudinary.com"],
+'media-src': ["'self'", "data:", "blob:", "https:", "res.cloudinary.com"],
+
           upgradeInsecureRequests: null,
         },
       },
--- config/server.js
+++ config/server.js
@@ -1,17 +1,9 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-
   url: env('PUBLIC_URL', 'https://digital-shop-backend-production.up.railway.app'),
-
   proxy: true,
-
   app: {
     keys: env.array('APP_KEYS'),
-  },
-
-  // 👇 Add this
-  dirs: {
-    public: './public',
   },
 });
