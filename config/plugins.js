cfe2f100a7bdc7dedb7bde12a7358715861e703f
Khizr Abbas <khizer@example.com>
2025-09-18 19:47:47 +0500

    files

--- config/plugins.js
+++ config/plugins.js
@@ -1,17 +1,17 @@
 module.exports = ({ env }) => ({
   upload: {
     config: {
-      provider: 'cloudinary',
+      provider: "cloudinary",
       providerOptions: {
-        cloud_name: env('CLOUDINARY_NAME'),
-        api_key: env('CLOUDINARY_KEY'),
-        api_secret: env('CLOUDINARY_SECRET'),
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
       },
       actionOptions: {
         upload: {},
         delete: {},
       },
-      // 👇 Is line se Strapi responsive formats generate nahi karega
+      // 👇 Is line se responsive formats OFF ho jaayenge
       breakpoints: false,
     },
   },
